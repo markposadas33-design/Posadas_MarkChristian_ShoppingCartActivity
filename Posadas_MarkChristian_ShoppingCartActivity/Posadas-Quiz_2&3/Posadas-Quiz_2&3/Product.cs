9a9abd50eb19cc4486eef59e422cd686e5640cc0
markposadas33-design <markposadas33@gmail.com>
2026-04-16 07:28:32 +0800

    Commit Error: Wrong File Upload
    Same Changes.

---
+++ Posadas_MarkChristian_ShoppingCartActivity/Posadas-Quiz_2&3/Posadas-Quiz_2&3/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posadas_Quiz_2_3
+{
+    internal class Product
+    {
+        public int Id;
+        public string Name;
+        public double Price;
+        public int Stock;
+
+        public Product(int id, string name, double price, int stock)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Stock = stock;
+        }
+
+        public string DisplayProduct()
+        {
+            return $"{Id}. {Name} - ₱{Price} (Stock: {Stock})";
+        }
+    }
+}
