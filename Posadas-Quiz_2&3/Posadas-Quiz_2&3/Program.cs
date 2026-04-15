578367a1f32de1f80a8e401cca65b0b5cfc41f9a
markposadas33-design <markposadas33@gmail.com>
2026-04-15 22:13:17 +0800

    Created Arrays of Products
    Acting as Booze Section of the Shop

--- Posadas-Quiz_2&3/Posadas-Quiz_2&3/Program.cs
+++ Posadas-Quiz_2&3/Posadas-Quiz_2&3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Posadas_Quiz_2_3
+{
+    class Program
+    {
+        public void Main()
+        {
+            Product[] products = new Product[]
+            {
+                new Product(1, "San Miguel Pale Pilsen", 65, 20),
+                new Product(2, "Red Horse", 90, 15),
+                new Product(3, "Heineken", 120, 10),
+                new Product(4, "Jack Daniel's Whiskey", 1500, 5),
+                new Product(5, "Absolut Vodka", 1200, 5),
+                new Product(6, "Smirnoff Vodka", 1100, 6),
+                new Product(7, "Bacardi Rum", 950, 7),
+                new Product(8, "Jose Cuervo Tequila", 1300, 4)
+            };
+
+        }
+    }
+}
