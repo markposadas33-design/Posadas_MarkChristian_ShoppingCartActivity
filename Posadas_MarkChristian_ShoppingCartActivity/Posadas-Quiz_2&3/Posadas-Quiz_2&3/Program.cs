15ff9fd9cd6307fe58a74263cf8083ddb7a408fe
markposadas33-design <markposadas33@gmail.com>
2026-04-16 07:39:12 +0800

    Commit Error 2: Code that went missing found

--- Posadas_MarkChristian_ShoppingCartActivity/Posadas-Quiz_2&3/Posadas-Quiz_2&3/Program.cs
+++ Posadas_MarkChristian_ShoppingCartActivity/Posadas-Quiz_2&3/Posadas-Quiz_2&3/Program.cs
@@ -16,6 +16,54 @@
                 new Product(8, "Jose Cuervo Tequila", 1300, 4)
             };
 
+            bool shopping = true;
+
+            // loop for products
+            while (shopping)
+            {
+                // display Menu
+                Console.WriteLine("\n-----|PRODUCT MENU|-----");
+                for (int i = 0; i < products.Length; i++)
+                {
+                    Console.WriteLine(products[i].DisplayProduct());
+                }
+
+                int productID = 0;
+                bool validProduct = false;
+
+                // loop for product validation
+                while (!validProduct)
+                {
+                    Console.Write("Enter product number: ");
+                    string input = Console.ReadLine();
+
+                    // check if numeric
+                    if (!int.TryParse(input, out productID))
+                    {
+                        Console.WriteLine("Input must be numeric");
+                        continue;
+                    }
+
+                    // check if the product id is existing
+                    if (productID < 1 || productID > products.Length)
+                    {
+                        Console.WriteLine("Product ID doesn't exist");
+                        continue;
+                    }
+
+                    // checking the stock
+                    if (products[productID - 1].Stock <= 0)
+                    {
+                        Console.WriteLine("Out of Stock");
+                        continue;
+                    }
+
+                    // breaking the loop when it's already valid
+                    validProduct = true;
+                }
+
+                Product selectedProduct = products[productID - 1];
+            }
         }
     }
 }
